d8e44dc9038dec203efe3064e03da8e0124318b0
Helen Bailey <hbailey@mit.edu>
2021-08-05 11:14:58 -0400

    Refactor llama scripts and add tests
    Why these changes are being introduced:
    To streamline existing code and make it more reusable, and add more
    robust testing for all known error states.
    
    How this addresses that need:
    * Refactors s3 module into a single S3 class with class methods
    * Simplifies parameteres for S3 class methods
    * Updates docstrings and cli help text to include more detail
    * Adds handling of all known errors to cli
    * Adds unit and cli tests for all known success and failure scenarios
    * Update Makefile to only lint llama folder (and tests where
      appropriate)

--- llama/s3.py
+++ llama/s3.py
@@ -1,28 +1,39 @@
+from boto3 import client
 from s3_concat import S3Concat
 
 
-def concatenate_files(date, session, bucket, key_prefix, output_filename):
-    """Concatenates files with the provided key_prefix in the provided bucket in a
-    random order"""
-    job = S3Concat(
-        bucket,
-        output_filename,
-        None,
-        session=session,
-    )
-    job.add_files(f"{key_prefix}{date}")
-    job.concat()
+class S3:
+    """An S3 class that provides a generic boto3 s3 client for interacting with S3
+    objects, along with specific S3 functionality necessary for llama scripts"""
 
+    def __init__(self):
+        self.client = client("s3")
 
-def move_s3_file(client, old_bucket, old_key, new_bucket, new_key):
-    """Duplicate behavior of AWS CLI mv command by copying the source object to a new key
-    and then deleting the source object."""
-    client.copy_object(
-        Bucket=new_bucket,
-        CopySource=f"{old_bucket}/{old_key}",
-        Key=new_key,
-    )
-    client.delete_object(
-        Bucket=old_bucket,
-        Key=old_key,
-    )
+    def concatenate_files(self, bucket, prefix, output_filename):
+        """Concatenate all files with the provided prefix within an S3 bucket into a new
+        file with the provided output file name. The source files may be concatenated
+        in a random order, and the output file will be created in the same bucket as
+        the source files."""
+        job = S3Concat(
+            bucket=bucket,
+            key=output_filename,
+            min_file_size=None,
+            s3_client=self.client,
+        )
+        job.add_files(prefix)
+        job.concat()
+
+    def move_file(self, key, source_bucket, destination_bucket):
+        """Duplicate behavior of AWS CLI mv command by copying the object from the
+        source bucket to a destination bucket and then deleting the object from the
+        source bucket. Note that if the provided key already exists in the destination
+        bucket, this will replace that object with the object from the source bucket."""
+        self.client.copy_object(
+            Bucket=destination_bucket,
+            CopySource=f"{source_bucket}/{key}",
+            Key=key,
+        )
+        self.client.delete_object(
+            Bucket=source_bucket,
+            Key=key,
+        )
--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,48 +1,102 @@
-from llama import s3
+import pytest
+
+from llama.s3 import S3
 
 
-def test_concatenate_files(s3_session, bucket_env):
-    # today's date
+def test_concatenate_files_no_files_with_prefix_exist(mocked_s3):
+    s3 = S3()
+    with pytest.raises(KeyError):
+        s3.concatenate_files("ils-sftp", "fake_prefix", "result.mrc")
+
+
+def test_concatenate_files_bucket_does_not_exist(mocked_s3):
+    s3 = S3()
+    with pytest.raises(s3.client.exceptions.NoSuchBucket):
+        s3.concatenate_files(
+            "fake_bucket",
+            "exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20210101",
+            "result.mrc",
+        )
+
+
+def test_concatenate_files_output_file_already_exists(mocked_s3):
+    s3 = S3()
     s3.concatenate_files(
-        "20210101",
-        s3_session,
         "ils-sftp",
-        "exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__",
-        "ALMA_UPDATE_EXPORT_20210101.mrc",
+        "exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20210101",
+        "exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20201012_marc1.mrc",
     )
-    concatenated_file = s3_session.client("s3").get_object(
-        Bucket="ils-sftp", Key="ALMA_UPDATE_EXPORT_20210101.mrc"
+    concatenated_file = s3.client.get_object(
+        Bucket="ils-sftp",
+        Key="exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20201012_marc1.mrc",
     )
     assert concatenated_file["Body"].read() == b"MARC 001MARC 002"
 
-    # past date
+
+def test_concatenate_files_includes_expected_content(mocked_s3):
+    s3 = S3()
     s3.concatenate_files(
-        "20201012",
-        s3_session,
         "ils-sftp",
-        "exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__",
-        "ALMA_UPDATE_EXPORT_20201012.mrc",
+        "exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20210101",
+        "result.mrc",
     )
-    concatenated_file = s3_session.client("s3").get_object(
-        Bucket="ils-sftp", Key="ALMA_UPDATE_EXPORT_20201012.mrc"
-    )
-    assert concatenated_file["Body"].read() == b"MARC 003MARC 004"
+    concatenated_file = s3.client.get_object(Bucket="ils-sftp", Key="result.mrc")
+    assert concatenated_file["Body"].read() == b"MARC 001MARC 002"
 
 
-def test_move_s3_file(s3_session, bucket_env):
-    assert len(s3_session.client("s3").list_objects(Bucket="ils-sftp")["Contents"]) == 4
-    assert "Contents" not in s3_session.client("s3").list_objects(
-        Bucket="dip-ils-bucket"
+def test_move_file_key_does_not_exist(mocked_s3):
+    s3 = S3()
+    with pytest.raises(s3.client.exceptions.ClientError):
+        s3.move_file("fake_key", "ils-sftp", "dip-ils-bucket")
+
+
+def test_move_file_source_bucket_does_not_exist(mocked_s3):
+    s3 = S3()
+    with pytest.raises(s3.client.exceptions.NoSuchBucket):
+        s3.move_file(
+            "exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20210101_marc1.mrc",
+            "fake_bucket",
+            "dip-ils-bucket",
+        )
+
+
+def test_move_file_destination_bucket_does_not_exist(mocked_s3):
+    s3 = S3()
+    with pytest.raises(s3.client.exceptions.NoSuchBucket):
+        s3.move_file(
+            "exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20210101_marc1.mrc",
+            "ils-sftp",
+            "fake_bucket",
+        )
+
+
+def test_move_file_replaces_if_key_already_exists_in_destination_bucket(mocked_s3):
+    s3 = S3()
+    s3.client.put_object(
+        Bucket="dip-ils-bucket",
+        Key="exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20210101_marc1.mrc",
+        Body="I am already here!",
     )
-    s3.move_s3_file(
-        s3_session.client("s3"),
-        "ils-sftp",
+    s3.move_file(
         "exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20210101_marc1.mrc",
+        "ils-sftp",
         "dip-ils-bucket",
-        "ARCHIVE/ALMA_UPDATE_EXPORT__20201012_marc1.mrc",
     )
-    assert len(s3_session.client("s3").list_objects(Bucket="ils-sftp")["Contents"]) == 3
-    assert (
-        len(s3_session.client("s3").list_objects(Bucket="dip-ils-bucket")["Contents"])
-        == 1
+    replaced_file = s3.client.get_object(
+        Bucket="dip-ils-bucket",
+        Key="exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20210101_marc1.mrc",
     )
+    assert replaced_file["Body"].read() == b"MARC 001"
+
+
+def test_move_file_copies_and_deletes_file(mocked_s3):
+    s3 = S3()
+    assert len(s3.client.list_objects(Bucket="ils-sftp")["Contents"]) == 4
+    assert "Contents" not in s3.client.list_objects(Bucket="dip-ils-bucket")
+    s3.move_file(
+        "exlibris/Timdex/UPDATE/ALMA_UPDATE_EXPORT__20210101_marc1.mrc",
+        "ils-sftp",
+        "dip-ils-bucket",
+    )
+    assert len(s3.client.list_objects(Bucket="ils-sftp")["Contents"]) == 3
+    assert len(s3.client.list_objects(Bucket="dip-ils-bucket")["Contents"]) == 1
