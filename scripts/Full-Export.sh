bc7f5a6d1121b18f1c064fcdf814b0f3618ea826
zotoMIT <zoto@mit.edu>
2021-08-09 13:24:31 -0400

    Add email notification of job status
    This adds the SES send email command to let me know the job status.

--- scripts/Full-Export.sh
+++ scripts/Full-Export.sh
@@ -15,4 +15,6 @@ CURRENT_DATE="$(date +"%Y%m")01"
 /usr/bin/python3.8 -m pipenv --python 3.8 install --dev
 
 #run the full update, this should only happen on the second of the month for files generated on the first. 
-/usr/bin/python3.8 -m pipenv run llama concat-timdex-export --export_type FULL --date "$CURRENT_DATE" >> /mnt/alma/logs/timdex-concat.log 2>&1
+/usr/bin/python3.8 -m pipenv run llama concat-timdex-export --export_type FULL --date "$CURRENT_DATE" > /mnt/alma/logs/timdex-concat.log 2>&1
+
+aws ses send-email --region us-east-1 --from noreply@libraries.mit.edu --to zoto@mit.edu --subject "Concat Job Completed" --text file:///mnt/alma/logs/timdex-concat.log
