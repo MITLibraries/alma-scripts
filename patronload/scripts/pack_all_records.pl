da78f9e7638d41dc5647cdbe0a38c8a76182747f
gam5 <gam5@users.noreply.github.com>
2021-09-17 13:49:10 -0400

    Update pack_all_records.pl
        Now deletes all xml load files after zipping them up.
        
        Now excludes student load files that have a matching staff load file.

--- patronload/scripts/pack_all_records.pl
+++ patronload/scripts/pack_all_records.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 
-my $working_dir = uc(shift) || 'STAFF';
+my $working_dir = 'STAFF';
 
 my $date = `date +%Y%m%d%H%M%S`;
 chomp $date;
@@ -18,9 +18,12 @@ print FH "<?xml version='1.0' encoding='UTF-8'?>\n";
 print FH "<userRecords>\n";
 close FH;
 
+my %staff;
 foreach my $file (@files) {
 	system ("cat $file >> $outfile");
 	system ("echo >> $outfile");
+	system ("rm -f $file");
+	$staff{$file}++;
 }
 
 open FH, ">>$outfile" or die $!;
@@ -30,6 +33,44 @@ close FH;
 
 my $zipfile = $outfile;
 $zipfile =~ s/\.xml/.zip/;
-system ("zip $zipfile $outfile");
+system ("zip -q $zipfile $outfile");
+system ("rm -f $outfile");
 
+$zipfile =~ s|^\.\./||;
+print "$zipfile\n";
+
+$working_dir = 'STUDENT';
+
+chdir '..';
+chdir $working_dir;
+
+my @student_files = glob("*.xml");
+exit if !$student_files[0];
+
+$outfile = '../SEND/' . lc($working_dir) . '_' . $date . '.xml';
+open FH, ">$outfile" or die $!;
+
+print FH "<?xml version='1.0' encoding='UTF-8'?>\n";
+print FH "<userRecords>\n";
+close FH;
+
+foreach my $file (@student_files) {
+	unless ($staff{$file}) {
+    	system ("cat $file >> $outfile");
+    	system ("echo >> $outfile");
+    }
+	system ("rm -f $file");
+}
+
+open FH, ">>$outfile" or die $!;
+print FH "</userRecords>\n";
+
+close FH;
+
+$zipfile = $outfile;
+$zipfile =~ s/\.xml/.zip/;
+system ("zip -q $zipfile $outfile");
+system ("rm -f $outfile");
+
+$zipfile =~ s|^\.\./||;
 print "$zipfile\n";
