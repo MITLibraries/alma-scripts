9224154e2a42583c7c7641f18a3d64ad3e824581
zotoMIT <zoto@mit.edu>
2021-09-22 15:48:48 -0400

    Cron jobs and pipfile updates for the patron load
    # Subject
    This commit adds the cron jobs, and scripts around doing the patron load on the alma-sftp-ec2 instance.
    # Why
    To automate the patron load process on our alma-sftp instance
    # How
    I create a Patron-load.sh script that can be used to run the process.
    I update the pip file and pipfile.lock so that the scripts run correctly
    I create a cron job that runs it once per day.
    # relevant tickets -
    https://mitlibraries.atlassian.net/browse/IMP-2310

---
+++ scripts/Patron-load.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#source the environment variables here so that the script runs correctly for the cron user
+source /etc/profile
+
+#make the logs dir if it doesn't already exist
+mkdir /mnt/alma/logs
+
+#change to the alma-scripts directory
+cd /mnt/alma/alma-scripts/patronload
+
+#install the dependencies and dev tools for them
+/usr/bin/python3.8 -m pipenv --python 3.8 install
+
+#Run the staff load
+/usr/bin/python3.8 -m pipenv run python staff.py > /mnt/alma/logs/patron-load.log 2>&1
+
+#Run the student load
+/usr/bin/python3.8 -m pipenv run python student.py >> /mnt/alma/logs/patron-load.log 2>&1
+
+#make the folder if it doesn't already exist, this perl script errors out without the folder
+mkdir SEND
+
+#Run the "zip function" that also does a diff and makes sure the staff files take precedence 
+perl scripts/pack_all_records.pl >> /mnt/alma/logs/patron-load.log 2>&1
+
+#Sync to the s3 bucket s3://alma-sftp-prod/exlibris/PatronLoad/
+# MV also deletes the zip files if they are succesfully copied
+aws s3 mv SEND/ s3://$ALMA_BUCKET/exlibris/PatronLoad/ --exclude "*" --include "*.zip" --recursive  >> /mnt/alma/logs/patron-load.log 2>&1
