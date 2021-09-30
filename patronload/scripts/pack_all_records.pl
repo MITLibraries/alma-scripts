840802f14f5ec3bcde32216c5b023ae1f2f73cbe
Helen Bailey <hbailey@mit.edu>
2021-09-30 11:35:27 -0400

    Document patron load Perl scripts
    Why these changes are being introduced:
    To help with future migration to Python, we need to thoroughly
    understand what the remaining Perl scripts do in the patron load
    process.
    
    How this addresses that need:
    * Adds inline comments to the remaining Perl scripts in patronload/
      scripts documenting the code in detail
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IMP-2318

--- patronload/scripts/pack_all_records.pl
+++ patronload/scripts/pack_all_records.pl
@@ -1,76 +1,118 @@
 #!/usr/bin/perl -w
+# Use strict regex matching
 use strict;
 
+# Set variable for working directory to STAFF
 my $working_dir = 'STAFF';
 
+# Create date string
 my $date = `date +%Y%m%d%H%M%S`;
+# Ensure no newline at end of date string
 chomp $date;
 
+# Move into working directory (STAFF)
 chdir $working_dir;
 
+# Match all XML files in working directory
 my @files = glob("*.xml");
+# Exit gracefully if there are no matching files
 exit if !$files[0];
 
+# Create output file with correctly formatted name
 my $outfile = '../SEND/' . lc($working_dir) . '_' . $date . '.xml';
+# Open output file or exit script if error
 open FH, ">$outfile" or die $!;
 
+# Add XML header and root element to output file and close file
 print FH "<?xml version='1.0' encoding='UTF-8'?>\n";
 print FH "<userRecords>\n";
 close FH;
 
+# Empty variable to store staff records
 my %staff;
+# Loop through all files in STAFF directory that end with .xml (as identified above).
+# There is one staff record per file from a previous script's output.
 foreach my $file (@files) {
+  # Concatenate the staff record file's contents onto the existing output file's
+  # contents
 	system ("cat $file >> $outfile");
+  # Print to stdout (informational)
 	system ("echo >> $outfile");
+  # Delete the staff record file that was just added to the output file
 	system ("rm -f $file");
+  # Also add the staff record to the staff records variable
 	$staff{$file}++;
 }
 
+# Open outfile file and add closing tag for XML root element, close output file
 open FH, ">>$outfile" or die $!;
 print FH "</userRecords>\n";
 
 close FH;
 
+# Zip the output file
 my $zipfile = $outfile;
 $zipfile =~ s/\.xml/.zip/;
 system ("zip -q $zipfile $outfile");
+# Delete the unzipped output file
 system ("rm -f $outfile");
 
+# Print zipped file name to stdout (informational)
 $zipfile =~ s|^\.\./||;
 print "$zipfile\n";
 
+# Change working directory to STUDENT
 $working_dir = 'STUDENT';
 
+# Move to new working directory
 chdir '..';
 chdir $working_dir;
 
+# Match all XML files in working directory
 my @student_files = glob("*.xml");
+# Exit gracefully if there are no matching files
 exit if !$student_files[0];
 
+# Create new output file name for student records
 $outfile = '../SEND/' . lc($working_dir) . '_' . $date . '.xml';
+# Open output file or exit script if error
 open FH, ">$outfile" or die $!;
 
+# Add XML header and root element to output file and close file
 print FH "<?xml version='1.0' encoding='UTF-8'?>\n";
 print FH "<userRecords>\n";
 close FH;
 
+# Loop through all files in STUDENT directory that end with .xml (as identified above).
+# There is one student record per file from a previous script's output.
 foreach my $file (@student_files) {
+  # Check whether student record already exists in staff records, if it does skip the
+  # record (we don't want to import student records twice)
 	unless ($staff{$file}) {
+      # Concatenate the student record file's contents onto the existing output file's
+      # contents
     	system ("cat $file >> $outfile");
+      # Print to stdout (informational)
     	system ("echo >> $outfile");
     }
+  # Delete the stduent record file (note that this happens whether the record was added
+  # to the output file or not)
 	system ("rm -f $file");
 }
 
+# Open outfile file and add closing tag for XML root element, close output file
 open FH, ">>$outfile" or die $!;
 print FH "</userRecords>\n";
 
 close FH;
 
+# Zip the output file
 $zipfile = $outfile;
 $zipfile =~ s/\.xml/.zip/;
 system ("zip -q $zipfile $outfile");
+# Delete the unzipped output file
 system ("rm -f $outfile");
 
+# Print zipped file name to stdout (informational)
 $zipfile =~ s|^\.\./||;
 print "$zipfile\n";
