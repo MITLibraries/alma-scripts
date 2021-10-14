bfd0005bd5b8182770bd63fc116d65db67126da6
zotoMIT <zoto@mit.edu>
2021-10-14 10:53:04 -0400

    Update bash scripts with improved email handling, improve patronload
    Update bash scripts with improved email handling, improve patronload with an rm of existing zip files if they are present.

--- scripts/Full-Export.sh
+++ scripts/Full-Export.sh
@@ -11,10 +11,12 @@ cd /mnt/alma/alma-scripts
 #Get the current date, but dont use the day, we hard code that to use the 1st
 CURRENT_DATE="$(date +"%Y%m")01"
 
-#install the dependencies and dev tools for them
-/usr/bin/python3.8 -m pipenv --python 3.8 install --dev
+#install the dependencies
+/usr/bin/python3.8 -m pipenv --python 3.8 install
 
 #run the full update, this should only happen on the second of the month for files generated on the first. 
 /usr/bin/python3.8 -m pipenv run llama concat-timdex-export --export_type FULL --date "$CURRENT_DATE" > /mnt/alma/logs/timdex-concat.log 2>&1
 
-aws ses send-email --region us-east-1 --from noreply@libraries.mit.edu --to lib-alma-timdex-notifications@mit.edu --subject "Concat Job Completed" --text file:///mnt/alma/logs/timdex-concat.log
+
+# IF its the PROD instance, send it to the prod email address, otherwise, send to just our dev emails
+[[ $WORKSPACE =~ .*prod* ]] && aws ses send-email --region us-east-1 --from noreply@libraries.mit.edu --to lib-alma-timdex-notifications@mit.edu --subject "PROD FULL Concat Job Completed" --text file:///mnt/alma/logs/timdex-concat.log || aws ses send-email --region us-east-1 --from noreply@libraries.mit.edu --to zoto@mit.edu --to hbailey@mit.edu --subject "TESTING FULL Concat Job Completed" --text file:///mnt/alma/logs/timdex-concat.log
--- scripts/Gitrepo-pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# CD to the git repo directory 
-cd /mnt/alma/alma-scripts
-
-# do a git pull 
-/usr/bin/git pull 
