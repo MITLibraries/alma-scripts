20903c1db26c9c90f2037119e74dc0e026354bda
Eric Hanson <ehanson@mit.edu>
2021-11-01 13:44:10 -0400

    Add new ssm methods
    * Add get_parameter_history method
    * Add update_parameter method
    * Add corresponding unit tests
    
    * https://mitlibraries.atlassian.net/browse/IMP-2336

--- llama/ssm.py
+++ llama/ssm.py
@@ -8,6 +8,14 @@ class SSM:
     def __init__(self):
         self.client = client("ssm", region_name="us-east-1")
 
+    def get_parameter_history(self, parameter_key):
+        """Get parameter history based on the specified key."""
+        response = self.client.get_parameter_history(
+            Name=parameter_key, WithDecryption=True
+        )
+        parameter_history = response["Parameters"]
+        return parameter_history
+
     def get_parameter_value(self, parameter_key):
         """Get parameter value based on the specified key."""
         parameter_object = self.client.get_parameter(
@@ -15,3 +23,10 @@ class SSM:
         )
         parameter_value = parameter_object["Parameter"]["Value"]
         return parameter_value
+
+    def update_parameter_value(self, parameter_key, new_value, parameter_type):
+        """Update parameter with specified value."""
+        response = self.client.put_parameter(
+            Name=parameter_key, Value=new_value, Type=parameter_type, Overwrite=True
+        )
+        return response
