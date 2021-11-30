b9111851fef89a5575e0f3305159e22f397d7871
Helen Bailey <hbailey@mit.edu>
2021-11-30 13:56:16 -0500

    Refactor to class-based config
    Why these changes are being introduced:
    We need to run some checks on the configuration settings at
    initialization to ensure production safety. Moving to class-based config
    simplifies both doing and testing this.
    
    How this addresses that need:
    * Refactors config.py module to use class-based config with
      implementation based on env
    * Adds checks for missing config values and production ssm path during
      config loading
    * Moves config instantiation, along with logging and sentry
      configuration, to __init__.py
    * Adds/updates tests for all changes
    
    Side effects of this change:
    There is now a single location in the code, the EXPECTED_CONFIG_VALUES
    dict in config.py, where all expected config settings are tracked. This
    is useful as both in-code documentation and to enable configuration
    checks for staging and production deployment.
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/ASI-28

---
+++ tests/test_config.py
@@ -0,0 +1,131 @@
+import botocore
+import pytest
+
+from llama.config import Config
+
+
+def test_prod_stage_config_success(mocked_ssm, monkeypatch):
+    monkeypatch.setenv("WORKSPACE", "stage")
+    config = Config()
+    assert config.ENV == "stage"
+    assert config.SSM_PATH == "/test/example/"
+    assert config.ALMA_API_URL == "http://example.com/"
+    assert config.DATA_WAREHOUSE_USER == "fake_dw_user"
+    assert config.DATA_WAREHOUSE_PASSWORD == "fake_dw_password"
+    assert config.DATA_WAREHOUSE_HOST == "dw.fake.edu"
+    assert config.DATA_WAREHOUSE_PORT == "0000"
+    assert config.DATA_WAREHOUSE_SID == "ABCDE"
+    assert config.LOG_LEVEL == "warning"
+    assert config.SENTRY_DSN == "sentry_123456"
+
+
+def test_prod_stage_config_missing_values_raises_error(mocked_ssm, monkeypatch):
+    monkeypatch.setenv("WORKSPACE", "stage")
+    mocked_ssm.put_parameter(
+        Name="/test/example/ALMA_API_URL",
+        Value="   ",
+        Overwrite=True,
+    )
+    with pytest.raises(Exception):
+        Config()
+
+
+def test_load_other_env_config_success():
+    config = Config()
+    assert config.ENV == "test"
+    assert config.SSM_PATH == "/test/example/"
+    assert config.ALMA_API_URL == "http://example.com/"
+    assert config.DATA_WAREHOUSE_USER == "test_dw_user"
+    assert config.DATA_WAREHOUSE_PASSWORD == "test_dw_password"
+    assert config.DATA_WAREHOUSE_HOST == "database.example.com"
+    assert config.DATA_WAREHOUSE_PORT == "5500"
+    assert config.DATA_WAREHOUSE_SID == "abcdef"
+    assert config.LOG_LEVEL == "INFO"
+    assert config.SENTRY_DSN is None
+
+
+def test_get_env_success():
+    env = Config.get_env()
+    assert env == "test"
+
+
+def test_get_env_without_workspace_raises_error(monkeypatch):
+    monkeypatch.delenv("WORKSPACE", raising=False)
+    with pytest.raises(Exception):
+        Config.get_env()
+
+
+def test_get_ssm_path_success():
+    ssm_path = Config.get_ssm_path("whatever")
+    assert ssm_path == "/test/example/"
+
+
+def test_get_ssm_path_raises_error_in_prod_stage(monkeypatch):
+    monkeypatch.delenv("SSM_PATH", raising=False)
+    with pytest.raises(Exception):
+        Config.get_ssm_path("stage")
+
+
+def test_get_ssm_path_returns_none_if_not_set_in_non_prod_stage_env(monkeypatch):
+    monkeypatch.delenv("SSM_PATH", raising=False)
+    ssm_path = Config.get_ssm_path("whatever")
+    assert ssm_path is None
+
+
+def test_check_sentry_raises_error():
+    config = Config()
+    config.SENTRY_DSN = "whatever"
+    with pytest.raises(ZeroDivisionError):
+        config.check_sentry()
+
+
+def test_check_sentry_does_nothing_if_not_set():
+    config = Config()
+    assert config.check_sentry() is None
+
+
+def test_get_alma_api_key_prod_stage(mocked_ssm, monkeypatch):
+    monkeypatch.setenv("WORKSPACE", "stage")
+    config = Config()
+    key = config.get_alma_api_key("ALMA_API_ACQ_READ_KEY")
+    assert key == "abc123"
+
+
+def test_get_alma_api_key_prod_stage_raises_error_if_not_provided(
+    mocked_ssm, monkeypatch
+):
+    monkeypatch.setenv("WORKSPACE", "stage")
+    config = Config()
+    with pytest.raises(botocore.exceptions.ClientError):
+        config.get_alma_api_key()
+
+
+def test_get_alma_api_key_dev_test():
+    config = Config()
+    key = config.get_alma_api_key("ALMA_API_ACQ_READ_KEY")
+    assert key == "abc123"
+
+
+def test_get_alma_api_key_dev_test_raises_error_if_not_provided():
+    config = Config()
+    with pytest.raises(KeyError):
+        config.get_alma_api_key()
+
+
+def test_missing_values_returns_missing_values(monkeypatch):
+    config = Config()
+    missing = config.missing_values()
+    assert missing == ["SENTRY_DSN"]
+
+
+def test_missing_values_returns_empty_list_if_all_present():
+    config = Config()
+    config.SENTRY_DSN = "something"
+    assert config.missing_values() == []
+
+
+def test_ssm_safety_check_raises_error(monkeypatch):
+    monkeypatch.setenv("WORKSPACE", "whatever")
+    monkeypatch.setenv("SSM_PATH", "/test/example/prod")
+    with pytest.raises(Exception):
+        Config()
