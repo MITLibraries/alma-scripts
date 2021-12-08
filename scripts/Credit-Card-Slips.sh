65a5c307ae69ff497b4a43ed8f21d10e1d8f31f5
zotoMIT <zoto@mit.edu>
2021-12-08 12:21:49 -0500

    Tweaks to scripts and update to process-invoices readme
    Tweak the scripts to be a bit more clearly stage/prod and update the invoice steps to not include specific python version.

--- scripts/Credit-Card-Slips.sh
+++ scripts/Credit-Card-Slips.sh
@@ -13,6 +13,6 @@ pipenv install
 
 #run the update, which automatically only uses the current days files
 # IF its the PROD instance, send it to the prod email address
-[[ $WORKSPACE == "prod" ]] && pipenv run llama cc-slips --source_email noreply@libraries.mit.edu --recipient_email ils-lib@mit.edu --recipient_email monoacq@mit.edu > /home/gituser/logs/credit-card-slips.log 2>&1 || /usr/bin/python3.8 -m pipenv run llama cc-slips --source_email noreply@libraries.mit.edu --recipient_email lib-alma-notifications@mit.edu > /home/gituser/logs/credit-card-slips.log 2>&1
+[[ $WORKSPACE == "prod" ]] && pipenv run llama cc-slips --source_email noreply@libraries.mit.edu --recipient_email ils-lib@mit.edu --recipient_email monoacq@mit.edu > /home/gituser/logs/credit-card-slips.log 2>&1 || pipenv run llama cc-slips --source_email noreply@libraries.mit.edu --recipient_email lib-alma-notifications@mit.edu > /home/gituser/logs/credit-card-slips.log 2>&1
 
-aws ses send-email --region us-east-1 --from noreply@libraries.mit.edu --to lib-alma-notifications@mit.edu --subject "Creditcardslips Job Completed" --text file:///home/gituser/logs/credit-card-slips.log
+aws ses send-email --region us-east-1 --from noreply@libraries.mit.edu --to lib-alma-notifications@mit.edu --subject "Creditcardslips $WORKSPACE Job Completed" --text file:///home/gituser/logs/credit-card-slips.log
